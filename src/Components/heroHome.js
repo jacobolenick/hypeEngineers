237f9c47301e46a9eeb12957c3bc76893576d67f
jacobolenick <jacobolenick@Jacobs-MacBook-Pro.local>
2021-05-26 11:19:50 -0700

    testing it it pushed

---
+++ src/Components/heroHome.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Hero extends Component {
+  render() {
+
+   return  <div className="Hero-Wrapper-Div">
+      <h2 className="Hero-h2">The minimal engineer</h2>
+    </div>
+
+  };
+};
+
+
+export default Hero;
